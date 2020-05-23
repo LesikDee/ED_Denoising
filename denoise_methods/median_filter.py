2b875ec8874e3de2c593f987fc6195b17767f7cd
Demian Lesik <lesikdjem@yandex.ru>
2020-05-24 01:42:06 +0300

    Add 2d median(average) filter implementation

--- denoise_methods/median_filter.py
+++ denoise_methods/median_filter.py
@@ -6,18 +6,14 @@ class MedianFilter(DenoiseMethod):
         super().__init__(data)
         self.sigma = sigma
 
-        self.f: int = 5
-        self.r: int = 21
-        self.h: int = 0.4 * sigma
-        self.q_arr_size = 10
+        self.window_size = 5
 
 
-    def execute_3d(self, data_3d: np.ndarray):
-        length = len(vd_3d_input)
-        height = len(vd_3d_input[0])
-        width = len(vd_3d_input[0][0])
-        print(height, width, length)
-        denoise_arr: np.ndarray = vd_3d_input.copy()
+    def execute_3d(self):
+        data = self.data
+        window3d_size = self.window_size
+        length, width, height = self.length, self.width, self.height
+        denoise_arr: np.ndarray = data.copy()
         edge = window3d_size // 2
 
         for x in range(edge, width - edge):
@@ -27,10 +23,36 @@ class MedianFilter(DenoiseMethod):
                     for i in range(window3d_size):
                         for j in range(window3d_size):
                             for k in range(window3d_size):
-                                cube_data.append(vd_3d_input[z + k  - edge][y + j - edge][x + i - edge])
+                                cube_data.append(data[z + k  - edge][y + j - edge][x + i - edge])
 
                     cube_data.sort()
                     denoise_arr[z][y][x] = cube_data[window3d_size * window3d_size * window3d_size // 2]
 
         return denoise_arr
 
+
+    def execute_2d(self):
+        data = self.data
+        window_size = self.window_size
+        length, width, height = self.length, self.width, self.height
+        denoise_arr: np.ndarray = data.copy()
+        edge = window_size // 2
+
+        pixels_in_window = window_size * window_size
+
+        for z in range(edge, length - edge):
+            for y in range(edge, height - edge):
+                average = 0
+                for j in range(window_size):
+                    for i in range(window_size):
+                        average += (data[z][y + j - edge][i])
+
+                denoise_arr[z][y][edge] = average / pixels_in_window
+
+                for x in range(edge + 1, width - edge):
+                    for j in range(window_size):
+                        average += (data[z][y + j - edge][x - edge] - data[z][y + j - edge][x + edge])
+
+                    denoise_arr[z][y][x] = average / pixels_in_window
+
+        return denoise_arr
